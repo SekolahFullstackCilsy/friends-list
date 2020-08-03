f808dd89b71956d3e19a89d356e1d927c3f39fae
saefullohmaslul <maslulsaefulloh@gmail.com>
2020-08-03 20:36:44 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Profile from './pages/profile'
-import Product from './pages/product'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Profile} />
-        <Route path='/product' component={Product} />
+        {/* <Route exact path='/' component={Profile} /> */}
       </Switch>
     </Router>
   )
